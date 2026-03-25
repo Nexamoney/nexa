2db2b44ed2344baa4f3a0bfc5fb2ee2282554f60
Peter Tschipper <peter.tschipper@gmail.com>
2026-03-25 13:00:38 -0300

    Only set _pcoinsDag if the tree->view is not null
      which could happen if the grove were initialized but the first
      subblock failed to connect as could happen on a fork.
    
      Instead if the view is null just use pcoinsTip. This prevents any possibility
      that we won't also set pcoinsDag to null.
    
    Set the fProcessed flag correctly and also reset the sequencdId where appropriate.

--- src/validation/dag.cpp
+++ src/validation/dag.cpp
@@ -472,6 +472,9 @@ bool CTailstormTree::Insert(CTreeNodeRef newNode)
                 }
                 else
                 {
+                    // Set the processed flag
+                    newNode->fProcessed = true;
+
                     // Set the tree pointer for the grove to the best tree in the grove
                     tailstormForest.SetDagCoinsTip();
                 }
@@ -479,9 +482,6 @@ bool CTailstormTree::Insert(CTreeNodeRef newNode)
 
             cvCommitQ.notify_all();
         }
-
-        // Set the processed flag
-        newNode->fProcessed = true;
 
         return true;
     }
@@ -1593,8 +1593,6 @@ void CTailstormForest::GenerateDagData(CTailstormGroveRef grove)
     auto chainparams = Params();
     auto &tree = grove->tree;
     {
-        uint32_t nSequenceId = 0;
-
         // Get the exclusion set for this dag which is use to pass to connect block and allow
         // processing to continue without a missing inputs error begin returned. This exlusion set is needed
         // because mapDagTxns, which is also used to skip processing a transaction twice,
@@ -1616,6 +1614,7 @@ void CTailstormForest::GenerateDagData(CTailstormGroveRef grove)
         std::vector<std::pair<uint256, CTreeNodeRef> > vSortedDag(tree->dag.begin(), tree->dag.end());
         std::sort(vSortedDag.begin(), vSortedDag.end(),
             [](const auto &a, const auto &b) { return a.second->nSequenceId < b.second->nSequenceId; });
+        uint32_t nSequenceId = 0;
         for (auto it = vSortedDag.begin(); it != vSortedDag.end(); it++)
         {
             nSequenceId++;
@@ -1660,7 +1659,11 @@ void CTailstormForest::GenerateDagData(CTailstormGroveRef grove)
                 tree->dag.erase(treenode->hash);
                 grove->mapGroveNodes.erase(treenode->hash);
                 nSequenceId--;
+                treenode->fProcessed = false;
+                treenode->nSequenceId = 0;
                 AddSubblockOrphan(treenode);
+                LOG(DAG, "%s():  Unable to process subblock while generating data: %s", __func__,
+                    treenode->hash.ToString());
             }
         }
     }
@@ -1695,7 +1698,8 @@ void CTailstormForest::SetDagCoinsTip()
         if (nTreeChainWork > nMaxChainWork)
         {
             nMaxChainWork = nTreeChainWork;
-            _pcoinsDag = grove->tree->view;
+            if (grove->tree->view)
+                _pcoinsDag = grove->tree->view;
         }
     }
 
