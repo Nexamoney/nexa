7a7c4c132c5a04c2e5609db10f8b8c03c684dc19
Andrew Stone <andrewstoneconsulting@gmail.com>
2026-03-27 12:09:49 +0000

    Solve nSequenceId==0 issue

--- src/validation/dag.h
+++ src/validation/dag.h
@@ -132,7 +132,8 @@ public:
     }
 
 protected:
-    bool Insert(CTreeNodeRef new_node);
+    // Returns new_node if inserted, the existing node if it was already inserted, or nullptr if insertion failed
+    CTreeNodeRef Insert(CTreeNodeRef new_node);
 };
 
 // All datamembers are protected by cs_forest
